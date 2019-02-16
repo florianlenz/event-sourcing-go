bf4a60302002a518c0550442fde395093ec6383d
Florian <>
2019-02-16 18:34:18 +0100

    [event_registry] added method to transform a persisted event into an event sourcing event

--- event_registry/registry.go
+++ event_registry/registry.go
@@ -3,27 +3,73 @@ package event_registry
 import (
 	"errors"
 	"es/event"
+	"es/store"
+	"fmt"
 )
 
 // will create an event from the payload. A factory is usually registered in the context of an event
-type EventFactory func(payload event.Payload) event.IEvent
+type EventFactory func(payload event.Payload) event.IESEvent
 
 // registered event
 type registeredEvent struct {
-	event   event.IEvent
+	event   event.IESEvent
 	factory EventFactory
+}
+
+type eventToESEvent struct {
+	event    store.Event
+	response chan struct {
+		esEvent event.IESEvent
+		error   error
+	}
 }
 
 // add event command for state container
 type addEvent struct {
-	event        event.IEvent
+	event        event.IESEvent
 	eventFactory EventFactory
 	response     chan error
 }
 
+func (r *Registry) RegisterEvent(event event.IESEvent, eventFactory EventFactory) {
+
+	r.addEvent <- addEvent{
+		event:        event,
+		eventFactory: eventFactory,
+	}
+
+}
+
+func (r *Registry) EventToESEvent(e store.Event) (event.IESEvent, error) {
+
+	// response channel
+	responseChan := make(chan struct {
+		esEvent event.IESEvent
+		error   error
+	}, 1)
+
+	// ask state machine to convert the event to
+	r.eventToESEvent <- eventToESEvent{
+		event:    e,
+		response: responseChan,
+	}
+
+	// response
+	response := <-responseChan
+
+	// return error if there is an error
+	if response.error != nil {
+		return nil, response.error
+	}
+
+	return response.esEvent, nil
+
+}
+
 // the public registry it self
 type Registry struct {
-	addEvent chan addEvent
+	addEvent       chan addEvent
+	eventToESEvent chan eventToESEvent
 }
 
 func New() *Registry {
@@ -31,9 +77,12 @@ func New() *Registry {
 	// register event over this channel
 	addEvent := make(chan addEvent)
 
+	eventToESEvent := make(chan eventToESEvent)
+
 	// event registry
 	reg := &Registry{
-		addEvent: addEvent,
+		addEvent:       addEvent,
+		eventToESEvent: eventToESEvent,
 	}
 
 	go func() {
@@ -60,6 +109,29 @@ func New() *Registry {
 					factory: addEvent.eventFactory,
 				}
 
+			case eventToESEvent := <-eventToESEvent:
+
+				e := eventToESEvent.event
+
+				// fetch registered event
+				registeredEvent, exists := registeredEvents[e.Name]
+				if !exists {
+					eventToESEvent.response <- struct {
+						esEvent event.IESEvent
+						error   error
+					}{esEvent: nil, error: fmt.Errorf("no event registered for name: %s", e.Name)}
+					continue
+				}
+
+				// create event from payload
+				esEvent := registeredEvent.factory(e.Payload)
+
+				// send response back
+				eventToESEvent.response <- struct {
+					esEvent event.IESEvent
+					error   error
+				}{esEvent: esEvent, error: nil}
+
 			}
 
 		}
@@ -67,14 +139,5 @@ func New() *Registry {
 	}()
 
 	return reg
-
-}
-
-func (r *Registry) RegisterEvent(event event.IEvent, eventFactory EventFactory) {
-
-	r.addEvent <- addEvent{
-		event:        event,
-		eventFactory: eventFactory,
-	}
 
 }
