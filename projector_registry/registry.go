5f5171884e26bb8b8c554bc882291270dbd6b516
Florian <>
2019-02-17 14:29:51 +0100

    [proj_registry] added tests

--- projector_registry/registry.go
+++ projector_registry/registry.go
@@ -60,31 +60,36 @@ func New() *Registry {
 
 	go func() {
 
-		for {
+		registeredProjectors := map[string]proj.IProjector{}
 
-			registeredProjectors := map[string]proj.IProjector{}
+		for {
 
 			select {
 			case addProjector := <-registerProjectorChan:
 
+				responseChannel := addProjector.response
+				projectorToAdd := addProjector.projector
+
 				// make sure that projector doesn't exist
-				_, exists := registeredProjectors[addProjector.projector.Name()]
+				_, exists := registeredProjectors[projectorToAdd.Name()]
 				if exists {
-					addProjector.response <- fmt.Errorf("couldn't add projector since a projector with the same name got already registered")
+					addProjector.response <- fmt.Errorf("projector with name '%s' already registered", projectorToAdd.Name())
 					continue
 				}
 
 				// add projector
-				registeredProjectors[addProjector.projector.Name()] = addProjector.projector
-				addProjector.response <- nil
+				registeredProjectors[projectorToAdd.Name()] = projectorToAdd
+				responseChannel <- nil
 
 			case query := <-queryProjectorsByEvent:
 
 				filteredEvents := []proj.IProjector{}
 
 				for _, projector := range registeredProjectors {
-					if projector.InterestedInEvent(query.event) {
-						filteredEvents = append(filteredEvents, projector)
+					for _, e := range projector.InterestedInEvents() {
+						if e.Name() == query.event.Name() {
+							filteredEvents = append(filteredEvents, projector)
+						}
 					}
 				}
 
---
+++ projector_registry/test_event.go
@@ -0,0 +1,26 @@
+package projector_registry
+
+import "es/event"
+
+type testEvent struct {
+	name       string
+	payload    event.Payload
+	version    uint8
+	occurredAt int64
+}
+
+func (e testEvent) Name() string {
+	return e.name
+}
+
+func (e testEvent) Payload() event.Payload {
+	return e.payload
+}
+
+func (e testEvent) Version() uint8 {
+	return e.version
+}
+
+func (e testEvent) OccurredAt() int64 {
+	return e.occurredAt
+}
