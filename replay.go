991f16354b288c640c61dc7f4391b02e4200496b
Florian <>
2019-02-21 15:40:06 +0100

    [replay] added replay to project

---
+++ replay.go
@@ -0,0 +1,53 @@
+package es
+
+import (
+	"github.com/mongodb/mongo-go-driver/bson/primitive"
+	"github.com/mongodb/mongo-go-driver/mongo"
+)
+
+func Replay(logger ILogger, db *mongo.Database, projectorRegistry *projectorRegistry, eventRegistry *eventRegistry) <-chan error {
+
+	// collections
+	eventCollection := db.Collection("events")
+	projectorCollection := db.Collection("projectors")
+
+	// repositories
+	eventRepository := newEventRepository(eventCollection)
+	projectorRepository := newProjectorRepository(eventCollection, projectorCollection)
+
+	done := make(chan error, 1)
+
+	// processor
+	processor := newProcessor(projectorRegistry, eventRegistry, projectorRepository, eventRepository, logger, true)
+
+	// drop all projectors
+	if err := projectorRepository.Drop(); err != nil {
+		done <- err
+		return done
+	}
+
+	// start background re playing
+	go func() {
+
+		// map over the events an project them
+		err := eventRepository.Map(func(eventID primitive.ObjectID) {
+
+			// tell processor to process event
+			onProcessed := processor.Process(eventID)
+
+			// wait till it got processed
+			<-onProcessed
+
+		})
+
+		// finish replay
+		done <- err
+
+		// stop processor
+		processor.Stop()
+
+	}()
+
+	return done
+
+}
---
+++ replay_test.go
@@ -0,0 +1,84 @@
+package es
+
+import (
+	"context"
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/mongo"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestReplay(t *testing.T) {
+
+	var createDB = func() (*mongo.Database, error) {
+
+		// create client
+		client, err := mongo.Connect(context.TODO(), "mongodb://test11:test11@ds141815.mlab.com:41815/godb")
+		if err != nil {
+			return nil, err
+		}
+
+		// database
+		db := client.Database("godb")
+		err = db.Drop(context.Background())
+
+		return db, err
+	}
+
+	Convey("must replay events", t, func() {
+
+		// logger
+		logger := &testLogger{
+			errorChan: make(chan error, 10),
+		}
+
+		// db
+		db, err := createDB()
+		So(err, ShouldBeNil)
+		eventCollection := db.Collection("events")
+
+		// first event
+		_, err = eventCollection.InsertOne(context.Background(), bson.M{
+			"name": "user.registered",
+			"payload": bson.M{
+				"event": "one",
+			},
+		})
+		So(err, ShouldBeNil)
+
+		// second event
+		_, err = eventCollection.InsertOne(context.Background(), bson.M{
+			"name": "user.registered",
+			"payload": bson.M{
+				"event": "two",
+			},
+		})
+		So(err, ShouldBeNil)
+
+		// projector registry
+		projectorRegistry := newProjectorRegistry()
+
+		//  register test event
+		createdWithPayloadChan := make(chan map[string]interface{}, 2)
+		eventRegistry := newEventRegistry()
+		err = eventRegistry.RegisterEvent(testEvent{name: "user.registered"}, func(payload Payload) IESEvent {
+			createdWithPayloadChan <- payload
+			return nil
+		})
+		So(err, ShouldBeNil)
+
+		done := Replay(logger, db, projectorRegistry, eventRegistry)
+
+		// wait till the replay is done
+		<-done
+
+		So(<-createdWithPayloadChan, ShouldResemble, map[string]interface{}{
+			"event": "one",
+		})
+		So(<-createdWithPayloadChan, ShouldResemble, map[string]interface{}{
+			"event": "two",
+		})
+
+	})
+
+}
